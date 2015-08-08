803317724f058fb6d3eee10c3a80e3f36e222917
Rana R. Bharose <rb.rana1993@gmail.com>
2015-08-09 02:50:30 +0530

    System Wakeup :) :)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- 	
- 	def setup
- 		@user = User.new(name: "ram", email: "rb.rana1993@gmail.com")
- 	end
-
- 	test "should be valid" do
- 		assert @user.valid? #assert method, which in this case 
- 							#succeeds if @user.valid? returns true and fails if it returns false.		
- 	end
-
- 	test "name should be present" do
- 		@user.name = "     "
- 		assert_not @user.valid? #assert_not that resulting User object is not valid.
- 	end
-
+	
 end
