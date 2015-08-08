8c8614c98a3de8e60905346f6636b18df4217ba7
Rana R. Bharose <rb.rana1993@gmail.com>
2015-08-09 04:58:53 +0530

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+    validates :password, presence: true, length: {minimum: 6}
+
     has_secure_password # secure password machinery will be 
     					# => implemented using a single Rails method called has_secure_password
 end
