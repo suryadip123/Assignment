bc5716a6e7e9815bf597e09bef4b1ea501f83954
Rana R. Bharose <rb.rana1993@gmail.com>
2015-08-08 20:30:44 +0530

    Added Global Title for pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title = "News bulletin board application"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+				
 end
