51ec4c4ac414209f74e3ac0738087bcda854a379
Rana R. Bharose <rb.rana1993@gmail.com>
2015-08-09 12:59:05 +0530

    When submitting the signup form developed in this chapter, the name, email address, and password get sent over the network,

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+   config.force_ssl = true #When submitting the signup form developed in this chapter, the name, email address, and password get sent over the network,
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
