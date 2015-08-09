87112707ab59d627891fc8fb0a9e2897b7b40901
Rana R. Bharose <rb.rana1993@gmail.com>
2015-08-09 22:25:05 +0530

    Testing for in news table user id should be present

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,6 @@ ActiveRecord::Schema.define(version: 20150809140342) do
   add_index "news", ["user_id", "created_at"], name: "index_news_on_user_id_and_created_at"
   add_index "news", ["user_id"], name: "index_news_on_user_id"
 
-  create_table "testingbymes", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
--- test/models/news_test.rb
+++ test/models/news_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class NewsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+		def setup
+			@user = users(:ram)
+			@news = News.new(content: "sample content", user_id: @user.id)
+		end
+
+		test "should be valid" do
+			assert @news.valid?
+		end
+
+		test "user id should be present" do
+			@news.user_id = nil
+			assert_not @news.valid?
+		end
+
+
+end 
