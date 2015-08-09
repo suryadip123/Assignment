d8b3171e528ee7c2d2902b6db6762afa56c229b7
Rana R. Bharose <rb.rana1993@gmail.com>
2015-08-09 06:32:36 +0530

    Signup page for new user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+	#Why we use helper funcion ?
+	#Ans - method defined in any helper file are automatically avaliable in any view file
+
+
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
+
 end
