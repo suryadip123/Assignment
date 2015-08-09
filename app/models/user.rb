7e0c7c075125f97b58c2bef532ef2521e5e2af57
Rana R. Bharose <rb.rana1993@gmail.com>
2015-08-09 15:20:33 +0530

    Gemerating test cases for Fixer using Bcript and Tested all test cases passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ActiveRecord::Base
 
     has_secure_password # secure password machinery will be 
     					# => implemented using a single Rails method called has_secure_password
+
+   	# Returns the hash digest of the given string.
+  	def User.digest(string)
+    	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    	BCrypt::Password.create(string, cost: cost)
+  end
 end
